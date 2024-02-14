3366057d5285ef37bf1aa7cdc45f3e89184c460d
rlimlatjr <raullatjr@gmail.com>
2024-02-13 19:32:55 -0800

    commit

--- 03_String_Methods/01_string_methods.py
+++ 03_String_Methods/01_string_methods.py
@@ -1,88 +1,89 @@
-
+#
 username = 'admin1'
 # print(dir(username))
-
-# print(username.capitalize())
-# print(username.casefold())
-# print(username.center(20), "Hi")
-
-# user_input = input("Enter Username:").casefold().strip().replace(" ","")
+# print(username.capitalize()) #Output Admin1
+# print(username.casefold())#Output admin1
+# print(username.center(20), "Hi") # will return centered string with length we defined #Output:      admin1        Hi
+# user_input = input("Enter Username:").casefold().strip().replace(" ","") #casefold will ignore case, strip will remove leading and trailing whitespaces from the output
 # if user_input == username:
 #     print(f"Input {user_input} matched")
 # else:
 #     print("Not matched")
-########################################################################
-
+# ########################################################################
+#
 # ip = '192.168.0.1'
 # device = 'router'
 # print(f"IP Address is: {ip}\nDevice type is: {device}")
 
-########################################################
-'''find'''
+# ip = '192.168.0.1'
+# device = 'router'
+# print(f"IP Address is: {ip}\nDevice type is: {device}".format(ip,device))
+
+# ########################################################
+# '''find'''
 # print(username.index('2'))
 
-##############################################################
-'''is decimal'''
+# ##############################################################
+# '''is decimal'''
 # a_number = '\u0035'
 # print(a_number)
 # print(a_number.isdigit())
 # print(a_number.isdecimal())
-
-b_letter = 'A'
+# b_letter = 'A'
 # print(b_letter.isascii())
-##########################################################
-'''is identifier  a-z, A-Z, 0-9, _ : should not start with number'''
+# ##########################################################
+# '''is identifier  a-z, A-Z, 0-9, _ : should not start with number'''
 # an_identifier = 'A123a_'
 # print(an_identifier.isidentifier())
-##########################################################
-'''printable'''
-printable = 'Hey there'
+# ##########################################################
+# '''printable'''
+# printable = 'Hey there'
 # print(printable.isprintable())
-##########################################################
-'''join'''
-list1 = ['Cisco', "IOS", "17.3"]
+# ##########################################################
+# '''join'''
+# list1 = ['Cisco', "IOS", "17.3"]
 # print('-'.join(list1))
 # print('.'.join(list1))
-##########################################################
-'''ljust'''
+# ##########################################################
+# '''ljust'''
 # print("abc".ljust(18), '12345')
 # print("abcabcabc".ljust(18), '12345')
 # print("abcabc".ljust(18), '12345')
 # print("abcrtui".ljust(18), '12345')
-
-##########################################################
-'''maketrans'''
+#
+# ##########################################################
+# '''maketrans'''
 # message = "Hey there.."
 # trans = message.maketrans('e.', 'E!')
 # print(message.translate(trans))
 
-##########################################################
-'''partition'''
-message = "ip route 192.168.0.0"
+# ##########################################################
+# # '''partition'''
+# message = "ip route 192.168.0.0"
 # print(message.partition(" route "))
+# #
+# ##########################################################
+# '''replace'''
+# message = "Hello there"
+# print(message.replace("there", "world"))
 
-##########################################################
-'''replace'''
-message = "Hello there"
-print(message.replace("there", "world"))
-
-##########################################################
-'''split'''
-users = 'user1, user2, user3'
-user_list = users.split(', ')
+# ##########################################################
+# '''split'''
+# users = 'user1, user2, user3'
+# user_list = users.split(', ')
 # print(user_list)
 # for user in user_list:
 #     print(f"Username is: {user}")
-##########################################################
-'''splitlines'''
-print("user1\nuser2\nuser3".splitlines())
-
-##########################################################
-'''translate'''
-trans = {46: 33}
-print("Hey there..".translate(trans))
-
-##########################################################
+# # ##########################################################
+# '''splitlines'''
+# print("user1\nuser2\nuser3".splitlines())
+#
+# # ##########################################################
+# '''translate'''
+# trans = {46: 33}
+# print("Hey there..".translate(trans))
+# #
+# ##########################################################
 '''zfill'''
 print('abc'.zfill(5))
 
--- 03_String_Methods/03_generate_dynamic_filenames_with_timestamp.py
+++ 03_String_Methods/03_generate_dynamic_filenames_with_timestamp.py
@@ -1,11 +1,25 @@
-import datetime
+# import datetime
+#
+# with open('show_commands.txt') as sh_cmd:
+#     commands = sh_cmd.readlines()
+# # time_seq_no_commands
+# # print(commands)
+# now = datetime.datetime.now().replace(microsecond=0)
+# for cmd in enumerate(commands, start=1):
+#     file_name = f"{str(now).replace(' ',':')}_{str(cmd[0]).zfill(2)}_{cmd[1].replace(' ','_').strip()}.txt"
+#     with open(file_name, 'w') as cmd_data:
+#         cmd_data.write('test_data')
 
+import datetime
 with open('show_commands.txt') as sh_cmd:
     commands = sh_cmd.readlines()
-# time_seq_no_commands
+##time_seq_no_commands
+
 # print(commands)
 now = datetime.datetime.now().replace(microsecond=0)
-for cmd in enumerate(commands, start=1):
-    file_name = f"{str(now).replace(' ',':')}_{str(cmd[0]).zfill(2)}_{cmd[1].replace(' ','_').strip()}.txt"
+for cmd in enumerate(commands, start = 1):#enumerate will create a tuple, without the start=1, the tuple will start with 0
+    file_name = f"{str(now).replace(' ', ':')}_{cmd[0]}_{cmd[1].strip().replace(' ','')}.txt"
     with open(file_name, 'w') as cmd_data:
         cmd_data.write('test_data')
+
+
