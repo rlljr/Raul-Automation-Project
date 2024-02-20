52a9c54965d41d6185734cb2ed1ce8d6cbd5b1b8
rlimlatjr <raullatjr@gmail.com>
2024-02-19 16:25:20 -0800

    commit

--- Functions/get_mac_address_list.py
+++ Functions/get_mac_address_list.py
@@ -1,21 +1,29 @@
 import ssh_function
 import re
+import csv
 from  ssh_function import cisco_cmd_executor
+import pandas as pd
+
 hosts = ["172.16.20.154", "172.16.20.210"]
 show_run_command = ["show ver | i Base ethernet MAC Address"]
 mac_pattern = re.compile(r'Base ethernet MAC Address       : (\S+)')
-
-
-
 x=[]
-y=[]
+macs=[]
+my_dict = {}
+complete_list=[]
+
 for i in range(len(hosts)):
     x.append(cisco_cmd_executor(hosts[i],show_run_command))
     for z in range(len(x)):
-        print(i)
         mac_match = mac_pattern.search(x[z])
-    y.append(mac_match.group(1))
-    #print("ip address " + hosts[i] + "has mac address " + mac_match.group(1))
-print(y)
+    macs.append(mac_match.group(1))
+complete_list.append(hosts)
+complete_list.append(macs)
+
+dict = {'IP address' : hosts, "Mac Addresses" : macs}
+df =pd.DataFrame(dict)
+df.to_csv('mac_list.csv')
+
+
 
 
